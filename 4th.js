485169b9172b3278037b098fcc8b512d3d6f3acc
asifkhan2000-codee <asifononto2000@gmail.com>
2025-02-13 21:55:16 +0600

    4th task done

---
+++ 4th.js
@@ -0,0 +1,16 @@
+const prompt = require("prompt-sync")();
+let str = prompt("Enter a sentence: ");
+for (let i = 0; i < str.length; i++) {
+    if (str[i] === "x") {
+        str = str.replace(str[i], "y");
+    } else if (str[i] === "X") {
+        str = str.replace(str[i], "Y");
+    }
+}
+console.log(str);
+
+
+
+// second method
+str = str.replaceAll("x", "y").replaceAll("X", "Y");
+console.log(str)
