4ea311bacd97fb3c3cd2fdd60c818de2d05b093f
asifkhan2000-codee <asifononto2000@gmail.com>
2025-02-13 20:17:48 +0600

    1st task done

--- 1st.js
+++ 1st.js
@@ -1,11 +1,19 @@
 const prompt = require("prompt-sync")();
 let str = prompt("Enter something: ");
-str = str.split("");
-console.log(str);
-let count = 0;
-for (const index of str) {
-    if (index.toLowerCase() === "a") {
-        count++;
+// first method
+let count1 = 0;
+for (let i = 0; i < str.length; i++) {
+    if (str[i].toLowerCase() === "a") {
+        count1++;
     }
 }
-console.log("String has the letter `a` for", count, "times.");
+console.log("The above string has the letter `a`", count1, "times.");
+// second method
+str = str.split("");
+let count2 = 0;
+for (const index of str) {
+    if (index.toLowerCase() === "a") {
+        count2++;
+    }
+}
+console.log("The above string has the letter `a`", count2, "times.");
--- 2nd.js
+++ 2nd.js
@@ -1,1 +1,2 @@
-.
+const prompt = require("prompt-sync")();
+const str = prompt("Enter a paragraph");
