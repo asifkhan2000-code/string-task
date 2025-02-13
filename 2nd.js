a3249a7bae8d47a3f0fade7c4599df66ac95ef03
asifkhan2000-codee <asifononto2000@gmail.com>
2025-02-13 20:28:43 +0600

    2nd task done

--- 2nd.js
+++ 2nd.js
@@ -1,2 +1,14 @@
 const prompt = require("prompt-sync")();
-const str = prompt("Enter a paragraph");
+const str = prompt("Enter a paragraph: ");
+let count1 = 0;
+let count2 = 0;
+for (let i = 0; i < str.length; i++) {
+    if (str[i] === "a") {
+        count1++;
+    }
+    if (str[i] === "A") {
+        count2++;
+    }
+}
+console.log("Number of `a` in the sentence is:", count1);
+console.log("Number of `A` in the sentence is:", count2);
